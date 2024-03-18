c3a41e33ee6df0d5d132cfa947eb68509d6994c9
Benjamin Miller <benjamin.miller2001@gmail.com>
2024-03-18 12:41:59 -0500

    Update forms.py

--- forms.py
+++ forms.py
@@ -63,6 +63,18 @@ class AddEmployeeForm(FlaskForm):
     email_address = StringField("Email Address", validators=[DataRequired(), Email()])
     submit = SubmitField("Add Employee")
 
+class EditEmployeeForm(FlaskForm):
+    first_name = StringField("First Name", validators=[DataRequired()])
+    last_name = StringField("Last Name", validators=[DataRequired()])
+    phone_number = StringField("Phone Number", validators=[DataRequired(), Length(min=10, max=15)])
+    employee_address = StringField("Address", validators=[DataRequired()])
+    email_address = StringField("Email Address", validators=[DataRequired(), Email()])
+    submit = SubmitField("Edit Employee")
+
+    class DeleteEmployeeForm(FlaskForm):
+        first_name = StringField("First Name", validators=[DataRequired()])
+        last_name = StringField("Last Name", validators=[DataRequired()])
+        submit = SubmitField("Delete Employee")
 
 class RecordNewJobForm(FlaskForm):
     employee_id = SelectField("Employee", coerce=int, validators=[DataRequired()])
