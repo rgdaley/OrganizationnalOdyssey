5dc46ff83a6c337c2596fbc7ea49bc494890f73a
Benjamin Miller <benjamin.miller2001@gmail.com>
2024-04-24 10:55:25 -0500

    Testing Visualization
    May have to regress

--- static/visualization.js
+++ static/visualization.js
@@ -23,7 +23,7 @@ function updateInfoPanel(node) {
                         <strong>${data.type}: ${data.name}</strong><br>`;
 
     if (data.type === "Employer") {
-        content += `Address: ${data.address}<br>
+        content += `Address: ${data.headquarters_address}<br>
                     Start Date: ${data.start_date}<br>
                     End Date: ${data.end_date || 'Active'}<br>`;
         if (data.description) {
@@ -32,9 +32,22 @@ function updateInfoPanel(node) {
     } else if (data.type === "Employee") {
         content += `Email: ${data.email_address}<br>
                     Phone: ${data.phone_number}<br>
-                    Address: ${data.address}`;
+                    Address: ${data.employee_address}<br>
+                    Employment Records:<br>`;
+        data.employers.forEach(function(record) {
+            content += `- Employer: ${record.employer}<br>
+                        Job Title: ${record.jobTitle}<br>
+                        Start Date: ${record.startDate}<br>
+                        End Date: ${record.endDate || 'Active'}<br><br>`;
+        });
     } else if (data.type === "Institution") {
-        content += `Institution Name: ${data.name}`;
+        content += `Institution Name: ${data.institution_name}<br>
+                    Certifications Granted:<br>`;
+        data.certifications.forEach(function(cert) {
+            content += `- Certification: ${cert.certificationName}<br>
+                        Awarded To: ${cert.awardedTo}<br>
+                        Award Date: ${cert.awardDate}<br><br>`;
+        });
     }
     content += `</div>`;
 
