77e60188f4313e31180d15d81027a027b327e696
Benjamin Miller <benjamin.miller2001@gmail.com>
2024-04-25 14:59:54 -0500

    Update visualization.js

--- static/visualization.js
+++ static/visualization.js
@@ -19,19 +19,11 @@ function createChart(visualizationData) {
 function updateInfoPanel(node) {
     var infoPanel = document.getElementById('infoPanel');
     var data = node.getData();
-    var label = "";
-        if (node.type === "Employer") {
-            label = node.name;
-        } else if (node.type === "Employee") {
-            label = node.name;
-        } else if (node.type === "Institution") {
-            label = node.name;
-        }
     var content = `<div class="pb-5">
                         <strong>${data.type}: ${data.name}</strong><br>`;
 
     if (data.type === "Employer") {
-        content += `Address: ${data.address}<br>
+        content += `Address: ${data.headquarters_address}<br>
                     Start Date: ${data.start_date}<br>
                     End Date: ${data.end_date || 'Active'}<br>`;
         if (data.description) {
@@ -40,29 +32,11 @@ function updateInfoPanel(node) {
     } else if (data.type === "Employee") {
         content += `Email: ${data.email_address}<br>
                     Phone: ${data.phone_number}<br>
-                    Address: ${data.address}`;
+                    Address: ${data.employee_address}`;
     } else if (data.type === "Institution") {
-        content += `Institution Name: ${data.name}`;
+        content += `Institution Name: ${data.institution_name}`;
     }
     content += `</div>`;
 
     infoPanel.innerHTML = content;
-}
-
-function showNodeInfo(node) {
-    var info = "";
-    if (node.type === "Employer") {
-        info = "<h2>" + node.name + "</h2>" +
-               "<p><strong>Start Date:</strong> " + node.start_date + "</p>" +
-               "<p><strong>End Date:</strong> " + node.end_date + "</p>" +
-               "<p><strong>Description:</strong> " + node.description + "</p>";
-    } else if (node.type === "Employee") {
-        info = "<h2>" + node.name + "</h2>" +
-               "<p><strong>Phone Number:</strong> " + node.phone_number + "</p>" +
-               "<p><strong>Email Address:</strong> " + node.email_address + "</p>" +
-               "<p><strong>Address:</strong> " + node.address + "</p>";
-    } else if (node.type === "Institution") {
-        info = "<h2>" + node.name + "</h2>";
-    }
-    document.getElementById("infoPanel").innerHTML = info;
 }
