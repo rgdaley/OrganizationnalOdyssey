83b8c8430a90a2fa9dfca50101a1fccaa614bf7b
Benjamin Miller <benjamin.miller2001@gmail.com>
2024-04-25 15:20:43 -0500

    Revert "Update flask_app.py"
    This reverts commit 370bf9551ac0c06b60a0b3d8c89edf99cb148ded.

--- flask_app.py
+++ flask_app.py
@@ -257,7 +257,11 @@ def traverse_tree(node, data, visited_nodes):
             employer = Employer.query.get(record.theEmployer)
             if employer.id not in visited_nodes:
                 traverse_tree(employer, data, visited_nodes)
-    
+    elif isinstance(node, Institution):
+        for record in node.institutions:
+            employee = Employee.query.get(record.certAwardedTo)
+            if employee.id not in visited_nodes:
+                traverse_tree(employee, data, visited_nodes)
 
 
 def add_employer_node(employer, data):
